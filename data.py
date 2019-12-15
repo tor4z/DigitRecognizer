88d0369513035d748d887f9931cfc127c1efa163
tor4z <vwenjie@hotmail.com>
2019-12-15 14:55:45 +0800

    data augumentation

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import torch
+from torchvision import transforms
 import copy
 import numpy as np
 from torch.utils.data import Dataset
@@ -6,21 +7,41 @@ import pandas as pd
 
 
 class DigitDataset(Dataset):
-    def __init__(self, data, test=False):
+    def __init__(self, opt, data, test=False, validate=False):
         self.data = data
-        self.len = len(data)
+        self.data_size = opt.data_size
+        self.data_len = len(data)
         self.test = test
+        self.validate = validate
         self.eye = torch.eye(10)
+        self.transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.RandomResizedCrop(20),
+            transforms.RandomGrayscale(),
+            transforms.RandomRotation(20),
+            transforms.RandomPerspective(),
+            transforms.ToTensor()
+        ])
 
     def __len__(self):
-        return self.len
+        if not (self.test or self.validate):
+            return self.data_size
+        else:
+            return self.data_len
 
     def one_hot(self, label):
         if label > 9 or label < 0:
             raise ValueError(f'({label})label out of range.')
         return self.eye[label]
+    
+    def transforms(self, image):
+        image = self.transform(image)
+        return image
 
     def __getitem__(self, index):
+        if not (self.test or self.validate):
+            index = np.random.randint(0, self.data_len)
+
         if self.test:
             label = 0                   # to be predict
             image = self.data[index, :]
@@ -29,7 +50,10 @@ class DigitDataset(Dataset):
             image = self.data[index, 1:]
         
         image = torch.tensor(image).type(torch.float32).view(28, 28) / 255.0
-        image = image.unsqueeze(0)
+        if not (self.test or self.validate):
+            image = self.transforms(image)
+        else:
+            image = image.unsqueeze(0)
         label = self.one_hot(label)
 
         return index, image, label
@@ -55,9 +79,9 @@ def get_train_val_test_dataset(opt):
     train_data = all_train_data[train_ids, :]
     validate_data = all_train_data[validate_ids, :]
 
-    train_dataset = DigitDataset(train_data)
-    validate_dataset = DigitDataset(validate_data)
-    test_dataset = DigitDataset(test_data, test=True)
+    train_dataset = DigitDataset(opt, train_data)
+    validate_dataset = DigitDataset(opt, validate_data, validate=True)
+    test_dataset = DigitDataset(opt, test_data, test=True)
 
     return train_dataset, validate_dataset, test_dataset
 
